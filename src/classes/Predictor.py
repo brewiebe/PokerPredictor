81b21bff8ca7e1bc5efa215e8c5907a7189c8022
Daniel Papanek <papanek82@gmail.com>
2017-04-08 18:13:46 -0700

    Probability Win/Bust

--- src/classes/Predictor.py
+++ src/classes/Predictor.py
@@ -2,56 +2,47 @@ class Predictor:
     def __init__(self, game):
         self.__game = game
 
-    def probability_win(self, player_place):
-        player = self.__game.get_players()[player_place]
-        prob_19 = self.probability_hand(player, 19)
-        prob_20 = self.probability_hand(player, 20)
-        prob_21 = self.probability_hand(player, 21)
-        prob_over_21 = self.probability_bust(player)
-        return (prob_19 + prob_20 + prob_21) - prob_over_21
+    def probability_win(self, player, min_wanted=19, prob=0):
+        if min_wanted > 21:
+            return prob
+        y = self.probability_hand(player, min_wanted)
+        if y == 0:
+            return prob
 
-    def probability_hand(self, player, desired_hand):
+        return self.probability_win(player, min_wanted+1, prob+y)
+
+    def probability_bust(self, player, prob=0, x=22):
+        y = self.probability_hand(player, x, True)
+        if y == 0:
+            return prob
+        else:
+            return self.probability_bust(player, prob+y, x+1)
+
+    def probability_hand(self, player, desired_hand, bust=False):
         player_hands = player.total()
         probability = 0
 
         for hand in player_hands:
-            if desired_hand < hand:
+            if desired_hand <= hand:
+                probability = 1
                 break
+
             card_needed = desired_hand - hand
-            if card_needed > 11:
-                break
-            num_card_needed = 0
+            if bust is False:
+                if card_needed > 11:
+                    continue
+            else:
+                if card_needed >= 11:
+                    continue
+
+            num_cards_known = 0
             for card in self.__game.get_exposed():
                 if card.get_int_value() == card_needed or (card_needed == 11 and card.get_int_value() == 1):
-                    num_card_needed += 1
+                    num_cards_known += 1
 
             if card_needed == 10:
-                probability += (4 - num_card_needed) / (52 - len(self.__game.get_exposed()))
+                probability += (16 - num_cards_known) / (52 - len(self.__game.get_exposed()))
             else:
-                probability += (16 - num_card_needed) / (52 - len(self.__game.get_exposed()))
-
-        return probability
-
-    def probability_bust(self, player):
-        player_hands = player.total()
-        probability = 0
-
-        for hand in player_hands:
-            # calculate card needed for getting 21
-            card_needed = 21 - hand
-
-            if card_needed > 11 or card_needed <= 0:
-                break
-            # anything higher than the card needed will cause a bust
-            for card in range(card_needed + 1, 11):
-                num_card_needed = 0
-                for ex_card in self.__game.get_exposed():
-                    if ex_card.get_int_value() == card or (card == 11 and ex_card.get_int_value() == 1):
-                        num_card_needed += 1
-
-                if card == 10:
-                    probability += (4 - num_card_needed) / (52 - len(self.__game.get_exposed()))
-                else:
-                    probability += (16 - num_card_needed) / (52 - len(self.__game.get_exposed()))
+                probability += (4 - num_cards_known) / (52 - len(self.__game.get_exposed()))
 
         return probability
